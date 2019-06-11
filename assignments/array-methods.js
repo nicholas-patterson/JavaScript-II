a30a93b1fd3c0002be791058ab311d30c604d62d
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-06-11 19:40:11 -0400

    Fixed my .reduce array method for challenge four

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -491,8 +491,8 @@ console.log(largeShirts);
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
 
-runners.reduce(function(total, runner) {
-  ticketPriceTotal.push(total + runner.donation);
+ticketPriceTotal = runners.reduce(function(total, runner) {
+  return (total += runner.donation);
 }, 0);
 console.log(ticketPriceTotal);
 
