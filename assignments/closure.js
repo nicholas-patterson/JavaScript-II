1c524da3f5d73007db4ab3cafbda445b17bce669
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-06-11 15:39:04 -0400

    Completed closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function myFunction() {
+  const name = "Nicholas";
+  function greetMe() {
+    console.log(`Hello ${name}`);
+  }
+  return greetMe;
+}
+let func = myFunction();
+func();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
